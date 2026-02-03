bed9877f0a973ce977786ca26a83b8420cf38103
fun-math <mallikakumar1729@gmail.com>
2026-02-03 14:40:21 +0000

    Deploying to gh-pages from @ fun-math/fun-math.github.io@0eb7caecf533b03bfbbc3d7a97d11c3232d2f3f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -538,9 +538,9 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-extremal-minimal-bipartite-matching-covered-graphs",
+          title: 'Extremal Minimal Bipartite Matching Covered Graphs',
+          description: "Part of my masters thesis",
           section: "Projects",handler: () => {
               window.location.href = "/projects/embmcg/";
             },},{id: "projects-project-1",
